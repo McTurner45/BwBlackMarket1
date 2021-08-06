3fdebd0b8baad12953ed1c77f8f255c3f5d3ed3f
Moses Chawawa <moseschawawa@gmail.com>
2021-08-07 01:19:07 +0200

    Added toggles

--- src/components/loan_details/loan_details.js
+++ src/components/loan_details/loan_details.js
@@ -1,10 +1,48 @@
 import './loan_details.css'
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { LoanContext } from "../../context";
 
 export default function LoanDetails() {
 
     const loanContext = useContext(LoanContext)
+
+    const [showEmployeeDetails, setShowEmployeeDetails] = useState(false);
+    const [showPersonalDetails, setShowPersonalDetails] = useState(false);
+
+    const employeeDetails = <div className="employee-details">
+        <h3>Employee Details</h3>
+        <p>Are you a government employee?</p>
+        <div className="employee-choices">
+
+            <span className="employee-choice">
+                <input type="radio" name="government_employee" value="yes" /> Yes
+                    </span>
+            <span className="employee-choice">
+                <input type="radio" name="government_employee" value="no" /> No
+            </span>
+        </div>
+    </div>
+
+    const personalDetails = <div className="personal-details">
+        <h3>Personnal Details</h3>
+        <div className="personal-details-values">
+            <input type="text" placeholder="Names" />
+            <input type="text" placeholder="Surname" />
+        </div>
+        <div className="personal-details-values">
+            <input type="text" placeholder="Phone Number" />
+            <input type="email" placeholder="Email Address" />
+        </div>
+        <div className="personal-details-values">
+            <input type="text" placeholder="ID Number" />
+            <input type="email" placeholder="Date of birth" />
+        </div>
+        <div className="personal-details-values">
+            <input type="text" placeholder="Marital Status" />
+            <input type="email" placeholder="Monthly Next Income" />
+        </div>
+    </div>
+
 
     return (
         <section>
@@ -35,45 +73,21 @@ export default function LoanDetails() {
                         <div className="button-proceed button-space">
                             <p>Change</p>
                         </div>
-                        <div className="button-proceed button-space">
+                        <div className="button-proceed button-space" onClick={() => setShowEmployeeDetails(!showEmployeeDetails)}>
                             <p>Proceed</p>
                         </div>
                     </div>
 
                 </div>
             </div>
-            <div className="employee-details">
-                <h3>Employee Details</h3>
-                <p>Are you a government employee?</p>
-                <div className="employee-choices">
+            {showEmployeeDetails ? employeeDetails : null}
+            {showEmployeeDetails ? personalDetails : null}
 
-                    <span className="employee-choice">
-                        <input type="radio" name="government_employee" value="yes" /> Yes
-                    </span>
-                    <span className="employee-choice">
-                        <input type="radio" name="government_employee" value="no" /> No
-                    </span>
+            <div className="loan-buttons">
+                <div className="button-proceed button-space">
+                    <p>Submit</p>
                 </div>
             </div>
-            <div className="personal-details">
-                <h3>Personnal Details</h3>
-                <div className="personal-details-values">
-                    <input type="text" placeholder="Names" />
-                    <input type="text" placeholder="Surname" />
-                </div>
-                <div className="personal-details-values">
-                    <input type="text" placeholder="Phone Number" />
-                    <input type="email" placeholder="Email Address" />
-                </div>
-                <div className="personal-details-values">
-                    <input type="text" placeholder="ID Number" />
-                    <input type="email" placeholder="Date of birth" />
-                </div>
-                <div className="personal-details-values">
-                    <input type="text" placeholder="Marital Status" />
-                    <input type="email" placeholder="Monthly Next Income" />
-                </div>
-            </div>
-        </section>
+        </section >
     );
 }

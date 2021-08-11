5b30239cdd640574b1701a087aa5774cef25f66f
Moses Chawawa <moseschawawa@gmail.com>
2021-08-11 23:42:57 +0200

    fixed toggle

--- src/components/loan_details/loan_details.js
+++ src/components/loan_details/loan_details.js
@@ -1,9 +1,9 @@
-import { useState } from 'react';
+import {useState} from 'react';
 import './loan_details.css'
 
 export default function LoanDetails({visibility, info}) {
 
-    const [personalDetailsVisible, setPersonalDetailsVisible] = useState(false)
+    const [employmentDetails, setEmploymentDetails] = useState(false)
 
     return (
         <section>
@@ -12,14 +12,16 @@ export default function LoanDetails({visibility, info}) {
                 <div className="loan-details-values">
                     <div className="loan-details-value">
                         <span> Loan Amount</span>
-                        <span><input readOnly="true" type="text" placeholder="BWP00000" value={`BWP ${info.loanNeeded}`} /></span>
+                        <span><input readOnly="true" type="text" placeholder="BWP00000"
+                                     value={`BWP ${info.loanNeeded}`}/></span>
                     </div>
-                    <hr />
+                    <hr/>
                     <div className="loan-details-value">
                         <span>Months to pay</span>
-                        <span><input readOnly="true"  type="text" placeholder="0 months" value={`${info.monthsToPay} months`} /> </span>
+                        <span><input readOnly="true" type="text" placeholder="0 months"
+                                     value={`${info.monthsToPay} months`}/> </span>
                     </div>
-                    <hr />
+                    <hr/>
                     {/* <div className="loan-details-value">
                         <span>Next payment date</span>
                         <span><input readOnly="true" type="date" value={info.} /> </span>
@@ -27,42 +29,47 @@ export default function LoanDetails({visibility, info}) {
                     {/* <hr /> */}
                     <div className="loan-details-value">
                         <span>Monthly Installments</span>
-                        <span><input type="text" /> </span>
+                        <span><input type="text"/> </span>
                     </div>
-                    <hr />
-                    <div className="button-proceed">
-                        <p>Save</p>
+                    <hr/>
+                    <div className="buttons">
+                        <div className="button-proceed button-space">
+                            <p>Change</p>
+                        </div>
+                        <div className="button-proceed button-space" onClick={()=>setEmploymentDetails(!employmentDetails)}>
+                            <p>Proceed</p>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div className="employee-details">
+            {employmentDetails ? <div className="employee-details">
                 <h3>Employee Details</h3>
                 <p>Are you a government employee?</p>
                 <div className="employee-choices">
                     <span className="employee-choice">
-                        <input type="radio" name="government_employee" value="yes" /> Yes
+                        <input type="radio" name="government_employee" value="yes"/> Yes
                     </span>
                     <span className="employee-choice">
-                        <input type="radio" name="government_employee" value="no" /> No
+                        <input type="radio" name="government_employee" value="no"/> No
                     </span>
                 </div>
-            </div>
-            { visibility.newCustomer ?  <div className="personal-details">
+            </div> : null}
+            {visibility.newCustomer && employmentDetails ? <div className="personal-details">
                 <h3>Personnal Details</h3>
                 <div className="personal-details-values">
-                    <input type="text" placeholder="Names" />
-                    <input type="text" placeholder="Surname" />
+                    <input type="text" placeholder="Names"/>
+                    <input type="text" placeholder="Surname"/>
                 </div>
                 <div className="personal-details-values">
-                    <input type="text" placeholder="Phone Number" />
-                    <input type="email" placeholder="Email Address" />
+                    <input type="text" placeholder="Phone Number"/>
+                    <input type="email" placeholder="Email Address"/>
                 </div>
                 <div className="personal-details-values">
-                    <input type="text" placeholder="ID Number" />
-                    <input type="email" placeholder="Date of birth" />
+                    <input type="text" placeholder="ID Number"/>
+                    <input type="email" placeholder="Date of birth"/>
                 </div>
                 <div className="personal-details-values">
-                    <input type="text" placeholder="Marital Status" />
+                    <input type="text" placeholder="Marital Status"/>
                     {/* <input type="email" placeholder="Monthly Next Income" /> */}
                 </div>
             </div> : null}

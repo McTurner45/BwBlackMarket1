fc6c369ebe7a10ee5a667f9a498417e99d944894
Moses Chawawa <moseschawawa@gmail.com>
2021-08-12 10:55:54 +0200

    Added submit button

--- src/components/loan_details/loan_details.js
+++ src/components/loan_details/loan_details.js
@@ -1,10 +1,16 @@
 import {useState} from 'react';
 import './loan_details.css'
+import Modal from "react-modal";
+import Button from "../header/button";
 
 export default function LoanDetails({visibility, info}) {
 
     const [employmentDetails, setEmploymentDetails] = useState(false)
     const [agreement, setAggrement] = useState(false)
+    const [isOpen, setIsOpen] = useState(false);
+    const showDialog = () => {
+        setIsOpen(!isOpen);
+    }
 
     return (
         <section>
@@ -40,17 +46,21 @@ export default function LoanDetails({visibility, info}) {
                     <br/>
                     <div>
                         <div>
-                            <input type='checkbox' checked={agreement} onChange={()=>setAggrement(!agreement)}/> I
+                            <input type='checkbox' checked={agreement} onChange={() => setAggrement(!agreement)}/> I
                             have read and understand the terms and conditions
                         </div>
                         {agreement ? <div className="buttons">
                             <div className="button-proceed button-space">
                                 <p>Change</p>
                             </div>
-                            <div className="button-proceed button-space"
-                                 onClick={() => setEmploymentDetails(!employmentDetails)}>
+                            {visibility.newCustomer ? (<div className="button-proceed button-space"
+                                                            onClick={() => setEmploymentDetails(!employmentDetails)}>
                                 <p>Proceed</p>
-                            </div>
+                            </div>) : (<div className="button-proceed button-space"
+                                            onClick={() => showDialog()}>
+                                <p>Submit</p>
+                            </div>)
+                            }
                         </div> : null}
                     </div>
 
@@ -86,7 +96,34 @@ export default function LoanDetails({visibility, info}) {
                     <input type="text" placeholder="Marital Status"/>
                     {/* <input type="email" placeholder="Monthly Next Income" /> */}
                 </div>
+                <br/>
+                <div className="button-proceed button-space"
+                     style={{
+                         display: 'flex',
+                         justifyContent: 'center'
+                     }}
+                     onClick={() => showDialog()}>
+                    <p>Submit</p>
+                </div>
             </div> : null}
+            <Modal
+                isOpen={isOpen}
+                onRequestClose={showDialog}
+                contentLabel="My dialog"
+                className="modal"
+                overlayClassName="myoverlay"
+            >
+
+                <div className="membership-modal">
+                    <div className="close" onClick={showDialog}>close</div>
+                    <div style={{textAlign: 'center'}} className="title"><h1>Thank you, please wait for a call from one
+                        of our agents</h1></div>
+                    <br/>
+                    <div className="proceed" style={{justifyContent: 'center'}}>
+                        <Button label="Done"/>
+                    </div>
+                </div>
+            </Modal>
         </section>
     );
 }

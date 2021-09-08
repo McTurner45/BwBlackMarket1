60b1e9ae4e084a8163c831fcb8e6897b3545e49f
AdmireKhulumo <admirekhulumo@gmail.com>
2021-09-08 09:17:07 +0200

    initialising from api-connect branch
    - re-initialise from api-connect branch
    - setup react query

--- src/App.js
+++ src/App.js
@@ -1,118 +1,125 @@
 import Header from "./components/header/header";
 import Calculator from "./components/calculator/calculator";
 import LoanDetails from "./components/loan_details/loan_details";
-import { useState } from "react";
-import { LoanContext } from "./context";
+import {useState} from "react";
+import {LoanContext} from "./context";
 import Landing from "./components/landing/landing";
 import Footer from "./components/footer/footer";
+import {QueryClient, QueryClientProvider} from "react-query";
+
+// for react query -- Create a client
+const queryClient = new QueryClient();
 
 function App() {
-  const [visibility, setVisibility] = useState({
-    newCustomer: false,
-    existingCustomer: false
-  });
-
-  const [info, setInfo] = useState({
-    loanNeeded: 0,
-    monthsToPay: 1,
-    grossSalary: 0,
-    netSalary: 0,
-    allowances: 0,
-    nextPaymentDate: Date("01/01/2022"),
-    monthlyLoanRepayment: 0,
-    nextPaymentDateAt: Date("01/01/2022"),
-    governmentEmployee: null,
-    names: null,
-    surname: null,
-    phoneNumber: null,
-    emailAddress: null,
-    omang: null,
-    dob: null,
-    maritalStatus: null
-  });
-
-  const [loanOptions, setLoanOptions] = useState();
-
-  const changeInfo = obj => {
-    setInfo(prev => {
-      return {
-        ...prev,
-        ...obj
-      };
+    const [visibility, setVisibility] = useState({
+        newCustomer: false,
+        existingCustomer: false
     });
-  };
 
-  const toggleInvisibleExisting = () => {
-    setVisibility(prev => {
-      return {
-        ...prev,
-        existingCustomer: true
-      };
+    const [info, setInfo] = useState({
+        loanNeeded: 0,
+        monthsToPay: 1,
+        grossSalary: 0,
+        netSalary: 0,
+        allowances: 0,
+        nextPaymentDate: Date("01/01/2022"),
+        monthlyLoanRepayment: 0,
+        nextPaymentDateAt: Date("01/01/2022"),
+        governmentEmployee: null,
+        names: null,
+        surname: null,
+        phoneNumber: null,
+        emailAddress: null,
+        omang: null,
+        dob: null,
+        maritalStatus: null
     });
-  };
 
-  const toggleInvisibleNew = () => {
-    setVisibility(prev => {
-      return {
-        ...prev,
-        newCustomer: true
-      };
-    });
-  };
+    const [loanOptions, setLoanOptions] = useState();
 
-  const calcLoanOptions = loanContext => {
-    let url = "http://10.16.32.26:443/calculateLoan";
-
-    let formData = {
-      Employer: "Central Government",
-      NetPay: "45000",
-      BasicSalary: "50000",
-      GrossSalary: "55000",
-      TotalFixedAllowances: "1000"
+    const changeInfo = obj => {
+        setInfo(prev => {
+            return {
+                ...prev,
+                ...obj
+            };
+        });
     };
 
-    console.log(formData);
-    fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(formData)
-    })
-      .then(response => response.text)
-      .then(responseText => {
-        alert(responseText);
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  };
+    const toggleInvisibleExisting = () => {
+        setVisibility(prev => {
+            return {
+                ...prev,
+                existingCustomer: true
+            };
+        });
+    };
 
-  // const calculatorComponent =
+    const toggleInvisibleNew = () => {
+        setVisibility(prev => {
+            return {
+                ...prev,
+                newCustomer: true
+            };
+        });
+    };
 
-  return (
-    <LoanContext.Provider value={{ ...info, changeInfo }}>
-      <div className="App">
-        <Header
-          toggleInvisibleExisting={toggleInvisibleExisting}
-          toggleInvisibleNew={toggleInvisibleNew}
-        />
-        {visibility ? (
-          <div className="calculator-section">
-            {/*{visibility.existingCustomer || visibility.newCustomer? <Calculator /> : null}*/}
-            <Calculator calcLoanOptions={calcLoanOptions} />
-          </div>
-        ) : null}
+    const calcLoanOptions = loanContext => {
+        let url = "http://10.16.32.26:443/calculateLoan";
 
-        {visibility.existingCustomer || visibility.newCustomer ? (
-          <LoanDetails visibility={visibility} info={info} />
-        ) : (
-          <Landing />
-        )}
-        {/* {<Footer />} */}
-      </div>
-    </LoanContext.Provider>
-  );
+        let formData = {
+            Employer: "Central Government",
+            NetPay: "45000",
+            BasicSalary: "50000",
+            GrossSalary: "55000",
+            TotalFixedAllowances: "1000"
+        };
+
+        console.log(formData);
+        fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(formData)
+        })
+            .then(response => response.text)
+            .then(responseText => {
+                alert(responseText);
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    };
+
+    // const calculatorComponent =
+
+    return (
+        // wrap everything in react query provider
+        <QueryClientProvider client={queryClient}>
+            <LoanContext.Provider value={{...info, changeInfo}}>
+                <div className="App">
+                    <Header
+                        toggleInvisibleExisting={toggleInvisibleExisting}
+                        toggleInvisibleNew={toggleInvisibleNew}
+                    />
+                    {visibility ? (
+                        <div className="calculator-section">
+                            {/*{visibility.existingCustomer || visibility.newCustomer? <Calculator /> : null}*/}
+                            <Calculator calcLoanOptions={calcLoanOptions}/>
+                        </div>
+                    ) : null}
+
+                    {visibility.existingCustomer || visibility.newCustomer ? (
+                        <LoanDetails visibility={visibility} info={info}/>
+                    ) : (
+                        <Landing/>
+                    )}
+                    {/* {<Footer />} */}
+                </div>
+            </LoanContext.Provider>
+        </QueryClientProvider>
+    );
 }
 
 export default App;
